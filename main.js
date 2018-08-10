4eef752b1732675c98130ddfb9a981c5507398ee
yojinlll <yojin1995@qq.com>
2018-08-10 23:46:48 +0800

    提交无缝轮播(代码优化版)

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-$('.images > img:nth-child(4)').addClass('enter')
-$('.images > img:nth-child(5)').addClass('enter')
+let n
 
-let n = 1
+getInit()
+
 setInterval(() => {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
     .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
+        makeEnter($(e.currentTarget))
     })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n+1))
     n += 1
 },3000)
 
-var IMG = document.querySelectorAll('.images > img')
-var p  = IMG.length
+
+
+
+
+
+// -----------------------
 
 function x(n){
+    var IMG = document.querySelectorAll('.images > img')
+    var p  = IMG.length
     if(n>p){
         n = n%p
         if(n===0){
@@ -25,4 +28,24 @@ function x(n){
         }
     }
     return n
+}
+
+function getInit(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
 }
